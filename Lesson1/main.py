93832ba0b2c4e658570a875510605f528a00d37a
Sandpitturtleee <karolmazur@onet.eu>
2025-03-16 13:58:15 +0100

    Zad1 finish

--- Lesson1/main.py
+++ Lesson1/main.py
@@ -3,10 +3,7 @@ from Lesson1.model_ai import ModelAI
 if __name__ == '__main__':
     #Zad1
     model1 = ModelAI.import_model_json(file_name="model.json")
-    print(model1.model_count)
-    print(model1.model_name)
-    print(model1.version)
-    #end
+    print(f"{model1.model_count} {model1.model_name} {model1.version}")
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
